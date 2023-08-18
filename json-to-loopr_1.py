e4820a31ff8a8bb7d65c5561408a95f9cb914357
softee-p <62172101+softee-p@users.noreply.github.com>
2023-08-18 21:17:10 +0300

    Update and rename json-to-loopr_1 to json-to-loopr_1.py

--- json-to-loopr_1.py
+++ json-to-loopr_1.py
@@ -1,8 +1,21 @@
 import os
 import json
 
+# This script is an example of how to edit a folder
+# of metadata.json to be compliant with the loopring format.
+# It takes a folder's CID containing other files and appends
+# the file name. Essentially you want your links to look like
+# ipfs://CID/1.json
+# ipfs://SAMECID/2.json
+# etc
+
+
 # Define the folder containing the JSON files
-folder_path = r"E:\PythonProjects\NFT\nft_folder"
+folder_path = r"E:\PythonProjects\NFT\json_folder"
+
+# Define traits to replace
+trait_to_search = "Trait"
+new_value = "New_Value"
 
 # Iterate through each file in the folder
 for filename in os.listdir(folder_path):
@@ -24,6 +37,18 @@ for filename in os.listdir(folder_path):
         if 'image' in data:
             image_filename = data['image']
             data['image'] = f"ipfs://YOUR_CID_HERE/{image_filename}"
+
+        # You can use the same code snipped from above for the 'animation_url'
+        #
+        #
+        #
+
+        # Search for the specific trait_type in the attributes
+        if 'attributes' in data:
+            attributes = data['attributes']
+            for attribute in attributes:
+                if attribute.get('trait_type') == trait_to_search:
+                    attribute['value'] = new_value
         
         # Modify the 'properties' field
         if 'properties' in data:
